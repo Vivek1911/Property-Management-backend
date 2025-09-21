5f618484e9f1302caf8085a071717bde181b7712
Vivek1911 <vivekvinayrameshdubey@gmail.com>
2025-09-21 16:23:05 +0530

    backend war file commit

--- src/main/java/com/example/Property/PropertyManagementApplication.java
+++ src/main/java/com/example/Property/PropertyManagementApplication.java
@@ -2,12 +2,18 @@ package com.example.Property;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PropertyManagementApplication {
+public class PropertyManagementApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PropertyManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyManagementApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PropertyManagementApplication.class);
+    }
 }
